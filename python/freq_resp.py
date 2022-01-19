53572348118fed8b915c61fa1ba93920c3c0b03d
Daniel Liu <hi.daniel.liu@gmail.com>
2022-01-18 20:46:36 -0500

    added frequency response graph generation

--- python/freq_resp.py
+++ python/freq_resp.py
@@ -5,8 +5,9 @@ FFT is used to calculate the frequency response of the object.
 
 import sounddevice as sd
 import numpy as np
-from scipy.fft import fft, ifft
+from scipy.fft import fft, fftfreq
 from scipy.io.wavfile import read, write
+import matplotlib.pyplot as plt
 
 # path = "audio/sweep17-22k_rate96Khz_42ms.wav"
 
@@ -20,9 +21,6 @@ fs, data = read(path)
 sd.default.samplerate = fs
 sd.default.channels = 2
 
-print("playing sound")
-print(data)
-
 # play and record responses
 trials = 8
 responses = []
@@ -30,6 +28,24 @@ for i in range(trials):
     resp = sd.playrec(data)
     sd.wait()
     responses.append(resp)
+
+# calculate fft of responses
+for i in range(len(responses)):
+    response = responses[i]
+    response = np.mean(response, axis=1) # convert stereo to mono
+    N = len(response)
+    resp_fft = fft(response)*1/N
+    resp_freq = fftfreq(N, 1/fs)
+    
+    # create plot of spectrum
+    plt.figure()
+    plt.plot(resp_freq[:N//2], 2*np.abs(resp_fft[:N//2]))
+    plt.grid()
+    plt.xlabel("Frequency")
+    plt.ylabel("Magnitude")
+    plt.savefig("data/response{0}.png".format(i))
+    plt.close()
+
 
 # TODO: print out graphs of the frequency response instead of raw response audio
 for i in range(len(responses)):
